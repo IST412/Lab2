5d26cef101eb407254accb96979ea8cebc09e537
Cody <Cody@75.102.87.42>
2015-09-28 15:18:27 -0400

    program file

---
+++ test/lab2/Program.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Cody
+ */
+public class Program {
+    
+    public int addition(int numberOne, int numberTwo){
+        int result = numberOne + numberTwo;
+        return result;
+    }
+    
+    public int substraction(int numberOne, int numberTwo){
+        int result = numberOne - numberTwo;
+        return result;
+    }
+    
+    public int multiply(int numberOne, int numberTwo){
+        int result = numberOne * numberTwo;
+        return result;
+    }
+        
+    public int division(int numberOne, int numberTwo){
+        int result = numberOne / numberTwo;
+        return result;
+    }
+}
